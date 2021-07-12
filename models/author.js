cf526ade861c0df1383b0cfea029e1f316ed23ba
Justin Caovan <jtcaovan@gmail.com>
2021-07-11 21:32:05 -0700

    added populatedb

--- models/author.js
+++ models/author.js
@@ -26,9 +26,7 @@ AuthorSchema
 });
 
 // Virtual for author's URL
-AuthorSchema
-.virtual('url')
-.get(function () {
+AuthorSchema.virtual('url').get(function () {
   return '/catalog/author/' + this._id;
 });
 
